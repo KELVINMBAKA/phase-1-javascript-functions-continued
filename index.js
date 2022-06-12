165e26c57aeeb8b34401cf089ed809d69889bec1
KELVINMBAKA <loverc832@gmail.com>
2022-06-12 16:41:31 +0300

    commit tree b1267e259f92dc8c7625d99f76a8cab4e0711d9f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(ToDo="roller-skate") {
+    return `This Saturday, I want to ${ToDo}!`
+  }
+  
+  let mondayWork = function(Todo="go to the office") {
+    return `This Monday, I will ${Todo}.`
+  }
+  // code your solution here
+function saturdayFun(ToDo="roller-skate") {
+  return `This Saturday, I want to ${ToDo}!`
+}
+
+let wrapAdjective = function(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
